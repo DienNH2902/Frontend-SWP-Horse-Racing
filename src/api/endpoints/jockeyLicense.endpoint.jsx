6ca36509afb841b7e19b634cf2c0f561606fd08f
tuananhjenbe <nguyentuananh200904@gmail.com>
2026-06-25 16:05:02 +0700

    add function add license for jockey

--- src/api/endpoints/jockeyLicense.endpoint.jsx
+++ src/api/endpoints/jockeyLicense.endpoint.jsx
@@ -1,6 +1,8 @@
 export const JOCKEY_LICENSE_ENDPOINTS = {
   ROOT: "/jockey-licenses",
+  ME: "/jockey-licenses/me",
   DETAIL: (id) => `/jockey-licenses/${id}`,
+  UPLOAD_LICENSE: "/upload/jockey-license",
   UPDATE_JOCKEY_STATUS: (profileId) =>
     `/jockey-licenses/admin/update-status/${profileId}`,
 };
